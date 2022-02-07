feb4a7591dd55fb1836977ef9b88cf691148828e
Joseph Ampadu <josephampadu549@gmail.com>
2022-02-07 22:39:18 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+project_titles = 10.times.map { Faker::Internet::domain_word }
+
+split = 0.4
+offset = project_titles.length * split
+
+project_users = {
+  "leo@example.com" => project_titles[0, offset],
+  "bob@example.com" => project_titles[offset..-1]
+}
+
+project_users.each do  |email, projects|
+  user = User.new(email: email, password: "secret")
+  projects.each { |title| user.projects.build(title: title) }
+  user.save
+end
